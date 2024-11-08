2f51fa95819a1488a9c0188557fad0f1b55649a2
DanielHadzhiev <danielhadziev1@gmail.com>
2024-11-08 20:15:06 +0200

    Inital Commit

--- basi website/src/main/java/org/example/basi/basi_website/model/entity/Order.java
+++ basi website/src/main/java/org/example/basi/basi_website/model/entity/Order.java
@@ -1,4 +1,19 @@
 package org.example.basi.basi_website.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "orders")
 public class Order {
+
+    public Order(){
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+
 }
--- basi website/src/main/java/org/example/basi/basi_website/model/entity/Product.java
+++ basi website/src/main/java/org/example/basi/basi_website/model/entity/Product.java
@@ -1,4 +1,66 @@
 package org.example.basi.basi_website.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "products")
 public class Product {
+    public Product() {
+    }
+
+    public Product(String name,
+                   String description,
+                   double price,
+                   String mainImageUrl,
+                   String size) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.mainImageUrl = mainImageUrl;
+        this.size = size;
+        this.imageUrls = new ArrayList<>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Setter
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Setter
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Setter
+    @Column(name = "price", nullable = false)
+    private double price;
+
+
+    @Setter
+    @Column(name = "main_image_url", nullable = false)
+    private String mainImageUrl;
+
+    @Setter
+    @Column(name = "size", nullable = false)
+    private String size;
+
+    @Setter
+    @OneToMany(fetch = FetchType.EAGER,
+            targetEntity = ImagePath.class,
+            mappedBy = "product")
+    private List<ImagePath> imageUrls;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
